11cbfacbcb8bbcef55a71679e4d630c87b7d1fec
wldn143 <wldn143@gmail.com>
2022-04-06 17:13:12 +0900

    web api 연동

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const port = 8080;
+
+app.use(express.json()); //json형식의 코드 처리할 수 있도록 함
+app.use(cors());
+
+app.get("/products", (req, res) => {
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS: ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
+});
+
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller } = body;
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과: ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다");
+    });
+});
+
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT: ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("상품 조회에 에러가 발생했습니다");
+    });
+});
+
+app.listen(port, () => {
+  console.log("서버야 돌아라~");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
+});
